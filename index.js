4a200b98931bd1636b40c9eb159ddf39181eddb6
Nada.Salha <128420328+NadaSalha@users.noreply.github.com>
2025-08-07 13:30:41 +0300

    implement frontend for /posts endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const userRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
